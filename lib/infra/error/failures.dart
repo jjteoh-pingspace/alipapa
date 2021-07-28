fee2efec5a312a3efb8c2f757ea0372a6b2b5b85
jjteoh <teoh.jia.jun@pingspace.co>
2021-07-28 11:14:43 +0800

    style: 🎸 stdize all interface-like class prefix I

--- lib/infra/error/failures.dart
+++ lib/infra/error/failures.dart
@@ -1,6 +1,6 @@
 import 'package:equatable/equatable.dart';
 
-abstract class Failure extends Equatable {
+abstract class IFailure extends Equatable {
   @override
   List<Object> get props => [];
 }
